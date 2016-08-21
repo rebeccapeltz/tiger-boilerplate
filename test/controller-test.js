8e8a37e387c6f5aabc495d53ec33f58043286b71
Rebecca Peltz <rebeccapeltz@gmail.com>
2016-08-21 11:38:50 -0700

    sample test

--- test/controller-test.js
+++ test/controller-test.js
@@ -7,20 +7,12 @@ describe('testing controllers used in routing', function() {
     angular.mock.module('demoApp');
     angular.mock.inject(($controller) => {
       this.homeController = new $controller('HomeController');
-      this.fullsizeController = new $controller('FullsizeController');
     });
   });
 
-  it('home controller should create a list of images', () => {
-    expect(this.homeController.images.length > 0).toEqual(true);
-    expect(this.homeController.images.length).toEqual(4);
+  it('home controller should create a list of messages', () => {
+    expect(this.homeController.messages.length > 0).toEqual(true);
+    expect(this.homeController.messages.length).toEqual(3);
   });
-  it('fullsize controller should create a list of images', () => {
-    expect(this.fullsizeController.images.length > 0).toEqual(true);
-    expect(this.fullsizeController.images.length).toEqual(4);
-  });
-  it('fullsize controller should be able to test for valid id', () => {
-    expect(this.fullsizeController.isValidId('x')).toEqual(false);
-    expect(this.fullsizeController.isValid(1)).toEqual(true);
-  });
+
 });
